c2ce18a5673d4502375080a33a1a2dd325b2b9be
rob-netzke <rob.netzke@gmail.com>
2023-01-27 10:52:50 -0800

    google new window

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { AiOutlineGoogle } from 'react-icons/ai'
 export default function SearchBar() {
   return (
     <div className='flex text-white col-span-3 justify-center items-center'>
-        <form role="search" method="get" id="searchform" class="searchform" action="https://www.google.co.uk/search?hl=en-GB&source=hp&q=">
+        <form role="search" method="get" id="searchform" class="searchform" action="https://www.google.co.uk/search?hl=en-GB&source=hp&q=" target="_blank">
           <span className='flex flex-row justify-center items-center'>
             <AiOutlineGoogle size={30} className="m-2"/>
             <input type="text" autocomplete="off" name="q" className='font-lato text-lg dark:bg-gray-800/80 bg-white/80 text-black rounded-xl px-4 py-1 dark:text-white shadow-lg shadow-gray-400 justify-center items-center focus:outline-none'></input>
